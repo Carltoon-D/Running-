4a3b70bc3180fcfe84b3092413e89bb45ede3b1b
Carlton Downes <downesca@sheridancollege.ca>
2021-03-06 22:47:30 -0500

    Cursor and prefab

---
+++ Runner/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{// the ray casting is to set up the cursor to hit the moving target
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+    Transform scope;
+    public AnimationCurve lerpCurve;
+    float move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scope = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//setting up the target to move left to right 
+        move += 0.2F * Time.deltaTime;
+        float time1 = Mathf.Lerp(-5, 5, lerpCurve.Evaluate(move));
+        Vector3 place = new Vector3(time1, 0);
+        scope.position = place;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == "Target")
+            {
+                Debug.Log("This is a Player");
+            }
+           
+        }
+    }
+}
---
+++ Runner/Assets/Scripts/cursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor : MonoBehaviour
+{
+    Transform scope;
+    Vector3 mouseposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scope = GetComponent<Transform>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Setting up the mouse position to connect to the cursor
+        mouseposition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+        mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
+        mouseposition.z = 0;
+        scope.position = mouseposition;
+
+    }
+}
