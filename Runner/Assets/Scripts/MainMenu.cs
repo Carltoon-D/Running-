453796e535f2e8f98cf94fba8a656ba0436507db
Carlton Downes <downesca@sheridancollege.ca>
2021-03-06 21:34:00 -0500

    created main menu
    made the main menu and load scene

---
+++ Runner/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the game scene
+    /// </summary>
+    public void loadgame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
