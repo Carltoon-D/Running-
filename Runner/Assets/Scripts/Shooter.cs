67e0e65218ab707c8078d3f6a8ecab64521627af
Carlton Downes <downesca@sheridancollege.ca>
2021-04-07 22:56:01 -0400

    Add gun and rotation

---
+++ Runner/Assets/Scripts/Shooter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject crosshairs;
+    public GameObject player;
+
+
+
+
+    private Vector3 shooterthing;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shooterthing = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        crosshairs.transform.position = new Vector2(shooterthing.x, shooterthing.y);
+
+        Vector3 difference = shooterthing - player.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+
+    }
+}
