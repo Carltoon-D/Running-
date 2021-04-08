42c16d652a6f96ddd3c2b7ffa59adb65cd45a91d
Carlton Downes <downesca@sheridancollege.ca>
2021-04-07 23:34:56 -0400

    Added raycast

--- Runner/Assets/Scripts/Shooter.cs
+++ Runner/Assets/Scripts/Shooter.cs
@@ -20,10 +20,11 @@ public class Shooter : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { // gets the mouse position and moves cross hair to mouse position
         shooterthing = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
         crosshairs.transform.position = new Vector2(shooterthing.x, shooterthing.y);
 
+        // makes the gun rotate towards the mouse position
         Vector3 difference = shooterthing - player.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
---
+++ Runner/Assets/Scripts/box.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Destroy(this.gameObject);
+             
+            }
+        }
+    }
+}
